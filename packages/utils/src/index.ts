1c435439cf944ed1e22d0e74da13eb8c6c665cd2
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-08-06 00:17:22 +0900

    型定義のユーティリティを追加

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,11 @@
 export type { Primitive, Falsy, Nullish } from './types/aliases'
+export type {
+  MatchTypeKeys,
+  ExactMatchTypeKeys,
+  NotMatchTypeKeys,
+  ExactNotMatchTypeKeys,
+  RequiredAtLeastOne,
+} from './types/utils'
 
 export * from './assertion'
 export * from './datetime'
