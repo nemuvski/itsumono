bb711af824089d8b3410f5e679aa584c4c6f51a8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-08-09 15:22:19 +0900

    replaceTabWithSpaces()のnumSpacesを1以上の整数値をとる仕様に変更

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -58,18 +58,18 @@ export function removeZWChars(str: string, options?: RemoveZWCharsOptions) {
  * 引数str中のタブをスペースに置換
  *
  * @param str
- * @param numSpaces スペース数（0以上の整数値をとる）
+ * @param numSpaces スペース数（1以上の整数値をとる）
  * @returns {string}
- * @throws {TypeError} numSpacesが0以上の整数値でない場合はエラー
+ * @throws {TypeError} numSpacesが1以上の整数値でない場合はエラー
  * @example
  * // 返値: te  st
  * replaceTabWithSpaces('te\tst', 2)
- * // 返値: test
- * replaceTabWithSpaces('te\tst', 0)
+ * // 返値: te st
+ * replaceTabWithSpaces('te\tst', 1)
  */
 export function replaceTabWithSpaces(str: string, numSpaces: number) {
-  if (!isPositiveInteger(numSpaces, true)) {
-    throw new TypeError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 0')
+  if (!isPositiveInteger(numSpaces, false)) {
+    throw new TypeError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 1')
   }
   const spaces = new Array(numSpaces).fill(' ').join('')
   return str.replace(/\t/g, spaces)
--- packages/utils/tests/string.spec.ts
+++ packages/utils/tests/string.spec.ts
@@ -65,13 +65,15 @@ describe('string.ts', () => {
   test('replaceTabWithSpaces()', () => {
     expect(replaceTabWithSpaces('\ttest', 2)).toBe('  test')
     expect(replaceTabWithSpaces('t\te\ts\tt', 1)).toBe('t e s t')
-    expect(replaceTabWithSpaces('t\te\ts\tt', 0)).toBe('test')
+    expect(() => {
+      replaceTabWithSpaces('t\te\ts\tt', 0)
+    }).toThrowError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 1')
     expect(() => {
       replaceTabWithSpaces('t\te\ts\tt', 1.1)
-    }).toThrowError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 0')
+    }).toThrowError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 1')
     expect(() => {
       replaceTabWithSpaces('t\te\ts\tt', -1)
-    }).toThrowError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 0')
+    }).toThrowError('replaceTabToSpace(): The argument numSpaces expects integer greater than or equal to 1')
   })
 
   test('replaceSpacesWithTab()', () => {
