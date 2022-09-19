73de3aded6cfd60832b981951d515c670c81df94
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-09-19 23:01:22 +0900

    フロントページの内容を更新

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react'
 import clsx from 'clsx'
-import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
@@ -15,11 +14,6 @@ function HomepageHeader() {
       <div className='container'>
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/docs/intro'>
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   )
