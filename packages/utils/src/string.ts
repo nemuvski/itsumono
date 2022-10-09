ea384728c686456b4a606c377285a90c6a3c3ef1
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-10-10 00:31:27 +0900

    cls()追加

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,6 +1,7 @@
+import { isBoolean, isNullish, isNumber, isString } from './assertion'
 import { isPositiveInteger } from './number'
 import type { Primitive } from './types/aliases'
-import type { NewLineCodeIdentifier, RemoveControlCharsOptions, RemoveZWCharsOptions } from './types/misc'
+import type { ClsOptions, NewLineCodeIdentifier, RemoveControlCharsOptions, RemoveZWCharsOptions } from './types/misc'
 
 /**
  * 引数str中の制御文字を取り除いて（空文字に置換）返却
@@ -254,4 +255,63 @@ export function strf<F extends Record<string, Primitive>>(
     // 埋め込むフィールドがない場合はそのままとする
     return substring
   })
+}
+
+/**
+ * 引数の値からclass属性の値を構築して返却
+ *
+ * オプションにより、構築時に処理を加えることが可能
+ * - trimPeriod: 先頭のピリオドを削除
+ *
+ * @param value
+ * @param options
+ * @returns {string}
+ * @see {@link https://www.npmjs.com/package/clsx} inspired by clsx
+ * @example
+ * // 返値: button button--lg
+ * cls('button button--lg')
+ *
+ * // 返値: button button--lg
+ * cls(['button', 'button--lg'])
+ *
+ * // 返値: button
+ * cls('.button', { trimPeriod: true })
+ *
+ * // 返値: button is-open
+ * const isOpen = true
+ * cls(['button', isOpen && 'is-open', !isOpen && 'is-close'])
+ *
+ * // 返値: button isOpen is-open
+ * const isOpen = true
+ * cls(['button', { isOpen, 'is-open': isOpen, 'is-close': !isOpen }])
+ *
+ * // 返値: hello
+ * cls({ hello: true, world: false })
+ */
+export function cls(
+  value:
+    | undefined
+    | null
+    | string
+    | number
+    | boolean
+    | Record<string, boolean | null | undefined>
+    | Array<undefined | null | string | number | boolean | Record<string, boolean | null | undefined>>,
+  options?: ClsOptions
+): string {
+  // null,undefined,真偽値はclassの値として出力しない
+  if (isNullish(value) || isBoolean(value)) return ''
+  if (isString(value) || isNumber(value)) return options?.trimPeriod ? String(value).replace(/\.+/g, '') : String(value)
+  let res = ''
+  if (Array.isArray(value)) {
+    value.forEach((n) => {
+      const childRes = cls(n, options)
+      if (childRes) res += (res && ' ') + childRes
+    })
+  } else {
+    for (const key in value) {
+      if (value[key]) res += (res && ' ') + key
+    }
+  }
+  return options?.trimPeriod ? res.replace(/\.+/g, '') : res
 }
--- packages/utils/tests/string.spec.ts
+++ packages/utils/tests/string.spec.ts
@@ -1,4 +1,5 @@
 import {
+  cls,
   escapeRegExpChars,
   removeControlChars,
   removeZWChars,
@@ -174,5 +175,19 @@ describe('string.ts', () => {
         }
       )
     ).toBe('He is Taro YAMADA. Taro is 25 years old.')
+  })
+
+  test('cls', () => {
+    expect(cls('box box--lg')).toBe('box box--lg')
+    expect(cls('.box')).toBe('.box')
+    expect(cls('.box', { trimPeriod: true })).toBe('box')
+    expect(cls([100, 100.01], { trimPeriod: true })).toBe('100 10001')
+    expect(cls({ hello: true, 'is-open': true, 'is-close': false })).toBe('hello is-open')
+    expect(cls([null, undefined, true, false])).toBe('')
+    expect(cls(['test', 100, { hello: true, 'is-open': true, 'is-close': false }])).toBe('test 100 hello is-open')
+
+    const isOpen = true
+    expect(cls(['button', isOpen && 'is-open', !isOpen && 'is-close'])).toBe('button is-open')
+    expect(cls(['button', { isOpen, 'is-open': isOpen, 'is-close': !isOpen }])).toBe('button isOpen is-open')
   })
 })
