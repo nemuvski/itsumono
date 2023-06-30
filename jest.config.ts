fa19c44b497024f95de9459b15a4cb11bc65000a
K.Utsunomiya <kii.utsunomiya@gmail.com>
2023-06-30 18:58:15 +0900

    depsアップデート

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import { InitialOptionsTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from 'ts-jest'
 
-const config: InitialOptionsTsJest = {
+const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   clearMocks: true,
   collectCoverage: false,
