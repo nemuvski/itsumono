a9a5fa9366969dc8b7235175764ca6e48159cc6c
K.Utsunomiya <kii.utsunomiya@gmail.com>
2023-08-30 18:00:22 +0900

    websiteワークスペースのeslintのルールを変更

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -2,4 +2,7 @@ module.exports = {
   root: true,
   ignorePatterns: ['build/**', '.turbo/**', '.typedoc/**', '.docusaurus/**'],
   extends: ['@local/eslint-config-custom'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+  },
 }
